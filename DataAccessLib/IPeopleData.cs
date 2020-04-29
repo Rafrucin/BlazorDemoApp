96cc98e267f0ff2fd370b514165f196b2ed5ad8a
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-04-29 20:12:24 +0100

    Add project files.

---
+++ DataAccessLib/IPeopleData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLib.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLib
+{
+    public interface IPeopleData
+    {
+        Task<List<PersonModel>> GetPeople();
+        Task InserPerson(PersonModel person);
+    }
+}
---
+++ DataAccessLib/PeopleData.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib
+{
+    public class PeopleData : IPeopleData
+    {
+        private readonly ISQLDataAccess _db;
+
+        public PeopleData(ISQLDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<PersonModel>> GetPeople()
+        {
+            string sql = "select * from dbo.People";
+
+            return _db.LoadData<PersonModel, dynamic>(sql, new { });
+        }
+
+        public Task InserPerson(PersonModel person)
+        {
+            string sql = @"insert into dbo.People (FirstName, LastName, EmailAddress)
+                            values (@FirstName, @LastName, @EmailAddress);";
+
+            return _db.SaveData(sql, person);
+        }
+    }
+}
